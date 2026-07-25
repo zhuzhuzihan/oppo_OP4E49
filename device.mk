5d3ea67509576dfe07a3c97249d3ea5be6d78c2f
zihansama2009-dot <zihansama2009@gmail.com>
2026-07-26 07:51:45 +0800

    Fix duplicate vendor security patch property

--- device.mk
+++ device.mk
@@ -4,6 +4,3 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31
